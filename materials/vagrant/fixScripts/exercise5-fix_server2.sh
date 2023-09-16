b3ccc060597582cc4d46db2ac85b1a17a6eda4dd
Amit Yablonski <jablonder@gmail.com>
2023-09-16 23:45:16 +0300

    wip fix 5

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -5,18 +5,13 @@ sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_
 systemctl restart sshd
 apt update
 apt install sshpass
-
 ssh-keygen -t rsa -q -N "" -f ~/.ssh/id_rsa
 ssh-keyscan -H server1 >> ~/.ssh/known_hosts
-
 #server2 can ssh to server1
-
 sshpass -p vagrant ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@server1
 cp ~/.ssh/id_rsa /home/vagrant/.ssh
 chmod 0444 /home/vagrant/.ssh/id_rsa
-
 #server1 to connect to server2
 scp -B vagrant@server1:~/.ssh/id_rsa.pub  ~/.ssh/tmp
-#cp ~/.ssh/authorized_keys /home/vagrant/.ssh
 cat ~/.ssh/tmp >> /home/vagrant/.ssh/authorized_keys
 rm ~/.ssh/tmp
